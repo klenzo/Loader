0e52b26891af7aacb16aea4fa279dfa5848b4c6f
klenzo <crea2luxe@gmail.com>
2016-05-12 22:54:07 +0200

    Init plugin

---
+++ assets/js/jquery.loaders.js
@@ -0,0 +1,11 @@
+$(function(){
+	$.fn.Loaders = function (options) {
+		console.log(options);
+
+		var theme = options.theme, speed = options.speed, duration = options.duration;
+
+	/*	console.log(theme);
+		console.log(speed);
+		console.log(duration); */
+	}
+});
